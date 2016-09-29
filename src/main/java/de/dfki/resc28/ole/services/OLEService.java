91dc22ba9eea6ab9dadb07a89fcbc9f0bc37d3ee
René Schubotz <rene.schubotz@dfki.de>
2016-09-29 10:29:42 +0200

    more RDF content types

--- src/main/java/de/dfki/resc28/ole/services/OLEService.java
+++ src/main/java/de/dfki/resc28/ole/services/OLEService.java
@@ -44,7 +44,7 @@ public class OLEService extends BaseService
 	}
 	
 	@POST
-	@Produces({ Constants.CT_TEXT_TURTLE, Constants.CT_APPLICATION_RDFXML, Constants.CT_APPLICATION_XTURTLE, Constants.CT_APPLICATION_JSON, Constants.CT_APPLICATION_LD_JSON })
+	@Produces({ Constants.CT_APPLICATION_JSON_LD, Constants.CT_APPLICATION_NQUADS, Constants.CT_APPLICATION_NTRIPLES, Constants.CT_APPLICATION_RDF_JSON, Constants.CT_APPLICATION_RDFXML, Constants.CT_APPLICATION_TRIX, Constants.CT_APPLICATION_XTURTLE, Constants.CT_TEXT_N3, Constants.CT_TEXT_TRIG, Constants.CT_TEXT_TURTLE })
 	public Response post( @HeaderParam(HttpHeaders.ACCEPT) @DefaultValue(Constants.CT_TEXT_TURTLE) final String acceptType, 
 						  @QueryParam("uri") String fileUri )
 	{
